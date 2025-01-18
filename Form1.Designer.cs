4c86eed95c31fa47cffa7ecdcd70284e48b7e0bd
Giovanni Voltan <187798689+giovol@users.noreply.github.com>
2025-01-19 00:43:16 +0100

    Add artwork

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,9 +34,10 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "CromSharp";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
